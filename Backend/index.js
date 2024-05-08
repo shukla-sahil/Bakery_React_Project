0d0b9e46f8f9dab160270b112fd6f75bbf2b8e1a
shukla-sahil <87928951+shukla-sahil@users.noreply.github.com>
2024-05-08 15:07:27 +0530

    last

--- Backend/index.js
+++ Backend/index.js
@@ -143,9 +143,6 @@ app.get("/show-reviews", async (req,res)=>{
       res.send({result :"no product found"})
   }
 })
-
-
-
 // Storage
 const Storage = multer.diskStorage({
   destination :'uploads',
